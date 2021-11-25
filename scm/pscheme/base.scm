f4e79bd3d4c7cd30829e74d7fb05da427bf78f0a
Peter Elliott <pelliott@ualberta.ca>
2021-11-25 16:04:29 -0700

    add getopt to pscheme's standard library

---
+++ scm/pscheme/base.scm
@@ -0,0 +1,53 @@
+(define-library (pscheme base)
+  (import (scheme base)
+          (scheme cxr)
+          (pscheme string))
+  (export getopt)
+  (begin
+
+    ;; grammar is of the form ((sym #\shortform hasvalue) ...)
+    ;; returns a list of the form ((key . value) ... ('rest arg ...))
+    (define (getopt opts grammar)
+      (define (match-single grammar ch)
+        (cond
+         ((null? grammar) #f)
+         ((char=? (cadar grammar) ch) (car grammar))
+         (else (match-single (cdr grammar) ch))))
+
+      (define (single-char-arg opts rest i l)
+        (cond
+         ((>= i l) (inner (cdr opts) rest))
+         ((match-single grammar (string-ref (car opts) i)) =>
+          (lambda (rule)
+            (cond
+             ((not (caddr rule))
+              (cons (cons (car rule) #t) (single-char-arg opts rest (+ 1 i) l)))
+             ((= i (- l 1))
+              (cons (cons (car rule) (cadr opts))
+                    (inner (cddr opts) rest)))
+             (else
+              (cons (cons (car rule) (substring (car opts) (+ i 1) l))
+                    (inner (cdr opts) rest))))))
+         (else (error "unknown option -" (string-ref (car opts) i)))))
+
+      (define (inner opts rest)
+        (cond
+         ((null? opts) (list (cons 'rest (reverse rest))))
+         ((string=? (car opts) "--") (list (cons rest (append (reverse rest) (cdr opts)))))
+         ((string-starts-with (car opts) "--")
+          (let ((rule (assoc (string->symbol (substring (car opts) 2))
+                             grammar)))
+            (cond
+             ((not rule) (error "unknown option " (car opts)))
+             ((caddr rule)
+              (cons (cons (car rule) (cadr opts))
+                    (inner (cddr opts) rest)))
+             (else
+              (cons (cons (car rule) #t)
+                    (inner (cdr opts) rest))))))
+         ((string-starts-with (car opts) "-")
+          (single-char-arg opts rest 1 (string-length (car opts))))
+         (else (inner (cdr opts) (cons (car opts) rest)))))
+      (inner (cdr opts) '()))
+
+    ))
--- scm/pscheme/string.scm
+++ scm/pscheme/string.scm
@@ -1,6 +1,7 @@
 (define-library (pscheme string)
   (import (scheme base))
-  (export string-join)
+  (export string-join
+          string-starts-with)
   (begin
 
     (define (string-join sep strings)
@@ -8,4 +9,10 @@
              (cons (car strings)
                    (map (lambda (str) (string-append sep str))
                         (cdr strings)))))
+
+    (define (string-starts-with str needle)
+      (define slen (string-length str))
+      (define nlen (string-length needle))
+      (and (>= slen nlen)
+           (string=? (substring str 0 nlen) needle)))
     ))
