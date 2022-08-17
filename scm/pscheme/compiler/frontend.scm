93ec9afcf3ee682310308cd0b11c528d81062c40
Peter Elliott <pelliott@ualberta.ca>
2022-08-16 21:54:31 -0600

    frontend: add back scoping

--- scm/pscheme/compiler/frontend.scm
+++ scm/pscheme/compiler/frontend.scm
@@ -56,11 +56,122 @@
         `(define ,(name) (lambda ,(args) ,@(body)))))
       (expression
        ((if ,expression ,expression) (test tbranch)
-        `(if ,(test) ,(tbranch) (begin)))))
+        `(if ,(test) ,(tbranch) (begin)))
+       ((,expression ,@expression) (fn args)
+        `(call ,(fn) ,@(args)))))
+
+    (define-record-type frame
+      (make-frame args rest-arg locals closure parent)
+      frame?
+      (args frame-args)
+      (rest-arg frame-rest-arg set-frame-rest-arg!)
+      (locals frame-locals set-frame-locals!)
+      (closure frame-closure set-frame-closure!)
+      (parent frame-parent))
+
+    (define current-frame (make-parameter '()))
+
+    (define (new-frame args rest-arg parent)
+      (make-frame args rest-arg
+                  (if rest-arg '((#f)) '())
+                  '()
+                  parent))
+
+    (define (define-var! sym)
+      (if (not (null? (current-frame)))
+          (set-frame-locals! (current-frame) (cons sym (frame-locals (current-frame))))))
+
+    (define (rest-arg arg-list)
+      (cond
+       ((symbol? arg-list) arg-list)
+       ((null? arg-list) #f)
+       ((pair? arg-list) (rest-arg (cdr arg-list)))
+       (else (error "malformed argument list" arg-list))))
+
+    (define (regular-args arg-list onto)
+      (if (or (null? arg-list)
+              (symbol? arg-list))
+          onto
+          (regular-args (cdr arg-list) (cons (car arg-list) onto))))
+
+    (define (lookup-var-frame! sym frame)
+      (cond
+       ((null? frame)                     (lookup-global sym))
+       ((eq? sym (frame-rest-arg frame))  '(stack 0))
+       ((member sym (frame-locals frame)) `(stack ,(- (length (member sym (frame-locals frame))) 1)))
+       ((member sym (frame-args frame))   `(arg ,(- (length (member sym (frame-args frame))) 1)))
+       ((assq sym (frame-closure frame))  (error "this is fake")) ;(assq sym (frame-closure frame)))
+       (else
+        (let ((parent-ref (lookup-var-frame! sym (frame-parent frame))))
+          (if (is-syntax? 'global parent-ref)
+              parent-ref
+              (let ((clos (member parent-ref (frame-closure frame))))
+                (if clos
+                    `(closure ,(- (length clos) 1))
+                    (begin
+                      (set-frame-closure! frame (cons parent-ref (frame-closure frame)))
+                      `(closure ,(- (length (frame-closure frame)) 1))))))))))
+
+    (define (lookup-var! sym)
+      (lookup-var-frame! sym (current-frame)))
+
+    (define-pass resolve-names (normal-scheme)
+      ($ identifier (i)
+         `(ref ,(lookup-var! i)))
+
+      (program-toplevel
+       ((define-library ,library-name ,@library-declaration) (name decls)
+        (parameterize ((current-library (lookup-library (name))))
+          `(define-library ,(name) ,@(decls)))))
+
+      (proc-toplevel
+       ((define ,identifier ,expression) (name expr)
+        (define-var! (name 'raw)) ; raw so the name is defined before we resolve it on the next line
+        `(define ,(name) ,(expr))))
+
+      (expression
+       ((lambda (,@identifier) ,@proc-toplevel) (args body)
+        (define arglist (args 'raw))
+        (parameterize ((current-frame (new-frame (regular-args arglist '()) (rest-arg arglist) (current-frame))))
+          (let* ((processed-body (body))
+                 (nlocals (length (frame-locals (current-frame)))))
+            `(closure
+              (lambda ,arglist
+                ,@(if (frame-rest-arg (current-frame))
+                      `((accumulate-rest ,(length (frame-args (current-frame))) (stack 0)))
+                      '())
+                (push-locals ,nlocals)
+                ,@processed-body)
+              ,@(reverse (frame-closure (current-frame)))))))
+
+       ((ffi-symbol ,symbol) (sym)
+        `(ref (ffi ,sym)))))
+
+    (define (accumulate-library-decls forms imports exports begins)
+      (if (null? forms)
+          (values imports exports begins)
+          (accumulate-library-decls (cdr forms)
+                              (if (is-syntax? 'import (car forms)) (append imports (cdar forms)) imports)
+                              (if (is-syntax? 'export (car forms)) (append exports (cdar forms)) exports)
+                              (if (is-syntax? 'begin (car forms)) (append begins (cdar forms)) begins))))
+
+    (define-pass to-backend-form (ref-scheme)
+      (program-toplevel
+       ((define-library ,library-name ,@library-declaration) (name decls)
+        (define-values (imports exports begins) (accumulate-library-decls (decls) '() '() '()))
+        `(define-library ,(name)
+           ,@(map (lambda (imprt) `(import ,imprt)) imports)
+           ,@(map (lambda (form) `(begin ,form)) begins)))
+
+       ((import ,@library-name) (names)
+        `(begin ,@(map (lambda (imprt) `(import ,imprt)) (names))))))
+
 
     (define frontend
       (concat-passes
        import-and-macroexpand
-       normalize-forms))
+       normalize-forms
+       resolve-names
+       to-backend-form))
 
     ))
--- scm/pscheme/compiler/languages.scm
+++ scm/pscheme/compiler/languages.scm
@@ -3,7 +3,9 @@
           (pscheme compiler util)
           (pscheme compiler nanopass))
   (export lscheme
-          normal-scheme)
+          normal-scheme
+          ref-scheme
+          backend-lang)
   (begin
 
     (define (unquoted-literal? form)
@@ -16,6 +18,7 @@
     (define lscheme
       (language
        ;; terminals
+       (symbol symbol?)
        (identifier symbol?)
        (number number?)
        (literal unquoted-literal?)
@@ -35,20 +38,21 @@
        (library-name
         (,@library-name-part))
        (library-name-part
-        ,identifier
+        ,symbol
         ,number)
        (library-declaration
         (export ,@identifier)
         (import ,@library-name)
         (begin ,@program-toplevel))
        (expression
+        (begin ,@expression)
         (lambda (,@identifier) ,@proc-toplevel)
         (if ,expression ,expression ,expression)
         (if ,expression ,expression)
         (set! ,identifier ,expression)
         (quote ,any)
-        (builtin ,identifier ,@expression)
-        (ffi-symbol ,identifier)
+        (builtin ,symbol ,@expression)
+        (ffi-symbol ,symbol)
         (,expression ,@expression) ;; procedure call
         ,identifier
         ,literal)))
@@ -63,6 +67,45 @@
          (define-syntax ,identifier ,any))
         (expression
          (if ,expression ,expression)
-         ,literal))))
+         (,expression ,@expression)
+         ,literal))
+       (+
+        (expression
+         (call ,expression ,@expression)))))
+
+    (define ref-scheme
+      (edit-language
+       normal-scheme
+       (-
+        (identifier symbol?)
+        (expression
+         (lambda (,@identifier) ,@proc-toplevel)
+         (ffi-symbol ,symbol)))
+       (+
+        (identifier
+         (ref (stack ,number))
+         (ref (arg ,number))
+         (ref (global ,library-name ,symbol))
+         (ref (closure ,number))
+         (ref (ffi ,symbol)))
+        (proc-toplevel
+         (accumulate-rest ,number ,any)
+         (push-locals ,number))
+        (expression
+         (closure ,expression ,@any)
+         (lambda (,@symbol) ,@proc-toplevel)))))
+
+    (define backend-lang
+      (edit-language
+       ref-scheme
+       (-
+        (program-toplevel
+         (define-library ,library-name ,@library-declaration)
+         (import ,@library-name)))
+       (+
+        (program-toplevel
+         (define-library ,library-name ,@program-toplevel)
+         (import ,library-name)))))
+
 
     ))
