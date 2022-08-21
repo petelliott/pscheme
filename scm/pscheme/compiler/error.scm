d04f5ea3b83536216125fa98977d52d4a942f600
Peter Elliott <pelliott@ualberta.ca>
2022-08-21 16:37:38 -0600

    add error span printing to (pscheme compiler error)

--- scm/pscheme/compiler/error.scm
+++ scm/pscheme/compiler/error.scm
@@ -1,6 +1,7 @@
 (define-library (pscheme compiler error)
   (import (scheme base)
           (scheme write)
+          (scheme file)
           (scheme process-context)
           (pscheme compiler file)
           (srfi 28))
@@ -16,6 +17,51 @@
       (span error-span)
       (fmt error-fmt)
       (args error-args))
+
+    (define (print-line-number n)
+      (display (make-string (- 5 (string-length (number->string n))) #\space))
+      (display n)
+      (display " "))
+
+    (define (print-line text n span colour)
+      (print-line-number n)
+      (cond
+       ((and (= n (span-sr span))
+             (= n (span-er span)))
+        (display (substring text 0 (span-sc span)))
+        (display colour)
+        (display (substring text (span-sc span) (span-ec span)))
+        (display "\x1b[0m")
+        (display (substring text (span-ec span) (string-length text))))
+       ((= n (span-sr span))
+        (display (substring text 0 (span-sc span)))
+        (display colour)
+        (display (substring text (span-sc span) (string-length text)))
+        (display "\x1b[0m"))
+       ((= n (span-er span))
+        (display colour)
+        (display (substring text 0 (span-ec span)))
+        (display "\x1b[0m")
+        (display (substring text (span-ec span) (string-length text))))
+       (else
+        (display colour)
+        (display text)
+        (display "\x1b[0m")))
+      (newline))
+
+    (define (print-span span colour)
+      (define (inner port n)
+        (define line (read-line port))
+        (cond
+         ((and (>= n (span-sr span))
+              (<= n (span-er span)))
+          (print-line line n span colour)
+          (inner port (+ n 1)))
+         ((< n (span-sr span))
+          (inner port (+ n 1)))))
+      (call-with-input-file (span-file span)
+        (lambda (port)
+          (inner port 1))))
 
     (define (print-error error)
       (parameterize ((current-output-port (current-error-port)))
@@ -34,8 +80,9 @@
         (display (apply format (error-fmt error)
                         (map (lambda (arg) (format "\x1b[1m~a\x1b[0m" arg))
                              (error-args error))))
-        ;; TODO: print the span
-        (newline)))
+        (newline)
+        (when span
+          (print-span span colour))))
 
     (define pscheme-error-span (make-parameter #f))
 
