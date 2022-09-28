a5d291295ef91189080e0280970aea02c45ca65b
Peter Elliott <pelliott@ualberta.ca>
2022-09-28 17:58:05 -0600

    runtime: add gdb extension section
    this doesn't work out of the box due to gdb's auto-load-safe-path

---
+++ runtime/gdb_exts.c
@@ -0,0 +1,9 @@
+#define DEFINE_GDB_PY_SCRIPT(script_name) \
+  asm("\
+.pushsection \".debug_gdb_scripts\", \"MS\",@progbits,1\n\
+.byte 1 /* Python */\n\
+.asciz \"" script_name "\"\n\
+.popsection \n\
+");
+
+DEFINE_GDB_PY_SCRIPT("../meta/pscheme_gdb.py")
