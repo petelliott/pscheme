886e1a50c239482de6408f9416263436ab42c6d8
Peter Elliott <pelliott@ualberta.ca>
2022-12-10 14:19:12 -0700

    pscheme scripts: same args

--- pscheme1.sh
+++ pscheme1.sh
@@ -2,6 +2,6 @@
 
 script_dir=$(dirname $(realpath $0))
 
-#perf record $script_dir/pscheme1 -I$script_dir/scm -L$script_dir/runtime/runtime.a $@
+#perf record --call-graph dwarf $script_dir/pscheme1 -I$script_dir/scm -L$script_dir/runtime/runtime.a $@
 #gdb --args $script_dir/pscheme1 -I$script_dir/scm -L$script_dir/runtime/runtime.a $@
 $script_dir/pscheme1 -I$script_dir/scm -L$script_dir/runtime/runtime.a $@
